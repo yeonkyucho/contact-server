9f356e0fcf88e8e6f6e27e6f3337d200b6f52e39
yeonkyucho <yeonkyucho@gmail.com>
2021-08-24 11:08:47 +0900

    heroku database set

--- prisma/migrations/20210824020711_contact/migration.sql
+++ prisma/migrations/20210824020711_contact/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
-CREATE TABLE "contact" (
+CREATE TABLE "Contact" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
-    "detail" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
 
     PRIMARY KEY ("id")
 );
